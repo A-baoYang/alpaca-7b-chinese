ab9398661dd2547480f9297c787f5fb6d2e506e5
A-baoYang <rubyyang0615@gmail.com>
2023-04-24 14:26:43 +0000

    [feature] serving add support on AutoModel, BloomForCasualLM

--- serve/model.py
+++ serve/model.py
@@ -3,46 +3,102 @@ import sys
 import click
 import torch
 from peft import PeftModel
-from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+from transformers import (
+    AutoModel,
+    AutoTokenizer,
+    BloomForCausalLM,
+    BloomTokenizerFast,
+    GenerationConfig,
+    LlamaForCausalLM,
+    LlamaTokenizer,
+)
 from utils import generate_prompt
+
+
+def decide_model(args, device_map):
+    ModelClass = namedtuple("ModelClass", ('tokenizer', 'model'))
+    _MODEL_CLASSES = {
+        "llama": ModelClass(**{
+            "tokenizer": LlamaTokenizer,
+            "model": LlamaForCausalLM,
+        }),
+        "chatglm": ModelClass(**{
+            "tokenizer": AutoTokenizer, #ChatGLMTokenizer,
+            "model":  AutoModel, #ChatGLMForConditionalGeneration,
+        }),
+        "bloom": ModelClass(**{
+            "tokenizer": BloomTokenizerFast,
+            "model": BloomForCausalLM,
+        }),
+        "Auto": ModelClass(**{
+            "tokenizer": AutoTokenizer,
+            "model": AutoModel,
+        })
+    }
+    model_type = "Auto" if args.model_type not in ["llama", "bloom", "chatglm"] else args.model_type
+    
+    if model_type == "chatglm":
+        tokenizer = _MODEL_CLASSES[model_type].tokenizer.from_pretrained(
+            args.base_model,
+            trust_remote_code=True
+        )
+        # todo: ChatGLMForConditionalGeneration revision
+        model = _MODEL_CLASSES[model_type].model.from_pretrained(
+            args.base_model,
+            trust_remote_code=True,
+            device_map=device_map
+        )
+    else:
+        tokenizer = _MODEL_CLASSES[model_type].tokenizer.from_pretrained(args.base_model)
+        model = _MODEL_CLASSES[model_type].model.from_pretrained(
+            args.base_model,
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+            device_map=device_map
+        )
+
+    if model_type == "llama":
+        tokenizer.pad_token_id = 0
+        tokenizer.padding_side = "left"  # Allow batched inference
+
+    if device_map == "auto":
+        model = PeftModel.from_pretrained(
+            model,
+            finetuned_weights,
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = PeftModel.from_pretrained(
+            model,
+            finetuned_weights,
+            device_map=device_map
+        )
+    return tokenizer, model
 
 
 class ModelServe:
     def __init__(
         self,
         load_8bit: bool = True,
+        model_type: str = "llama",
         base_model: str = "decapoda-research/llama-7b-hf",
         finetuned_weights: str = "../finetuned/llama-7b-hf_alpaca-en-zh",
     ):
+        args = locals()
+        namedtupler = namedtuple("args", tuple(list(args.keys())))
+        local_args = namedtupler(**args)
+        
         if torch.cuda.is_available():
             self.device = "cuda"
+            self.device_map = "auto"
             # self.max_memory = {i: "15GIB" for i in range(torch.cuda.device_count())}
             # self.max_memory.update({"cpu": "30GB"})
         else:
             self.device = "cpu"
-        self.tokenizer = LlamaTokenizer.from_pretrained(base_model)
-        if self.device == "cuda":
-            self.model = LlamaForCausalLM.from_pretrained(
-                base_model,
-                load_in_8bit=load_8bit,
-                torch_dtype=torch.float16,
-                device_map="auto",
-            )
-            self.model = PeftModel.from_pretrained(
-                self.model,
-                finetuned_weights,
-                torch_dtype=torch.float16,
-            )
-        else:
-            self.model = LlamaForCausalLM.from_pretrained(
-                base_model, device_map={"": self.device}, low_cpu_mem_usage=True
-            )
-            self.model = PeftModel.from_pretrained(
-                self.model,
-                finetuned_weights,
-                device_map={"": self.device},
-            )
+            self.device_map = {"": self.device}
 
+        self.tokenizer, self.model = decide_model(args=local_args, device_map=self.device_map)
+        
         # unwind broken decapoda-research config
         self.model.config.pad_token_id = self.tokenizer.pad_token_id = 0  # unk
         self.model.config.bos_token_id = 1
